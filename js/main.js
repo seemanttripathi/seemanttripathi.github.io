3aab76010cbfb1718eaae7c0d447d88d73998657
Seemant Tripathi <47049137+seemanttripathi@users.noreply.github.com>
2019-01-28 17:21:07 +0530

    First Website

---
+++ js/main.js
@@ -0,0 +1,39 @@
+<!--JQuery For Slider-->
+
+jQuery(document).ready(function(){
+
+	"use strict";
+	$('#slider-carousel').carouFredSel({
+		responsive:true,
+		width:'100%',
+		circular:true,
+		scroll:{
+			itself:1,
+			duration:500,
+			pauseOnHovar:true
+		},
+		auto:true,
+		itself:
+		{
+			visible:{
+				min:1,
+				max:1
+			},
+			height:"variable"
+		},
+		pagination:{
+			container:"slidepager",
+			pageAnchorBuilder:false
+		}
+	});
+	$(window).scroll(function(){
+		var top =$(window).scrollTop();
+		if(top>=60){
+			$("header").addClass('secondary');
+		}
+		else 
+			if ($("header").hasClass('secondary')){
+				$("header").removeClass('secondary');
+			}
+	});
+});
